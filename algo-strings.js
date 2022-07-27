f25a5a85678af1e38aa298c293268eb9f476b07c
jas112 <jas.sandmaster@gmail.com>
2022-07-27 10:20:32 -0700

    algo-strings updated

---
+++ algo-strings.js
@@ -0,0 +1,106 @@
+// 01. Reverse an array without affecting special characters
+
+reverseStr = (str) => {
+    console.log('@reverseStr...');
+
+    testChar = (c) => {
+        var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+        if(format.test(c)){
+            // console.log(`${c} => is a special character!`);
+            return true;
+        } else {
+            // console.log(`${c} => is not a special character!`);
+            return false;
+        }
+    }
+
+
+    let start = 0;
+    let end = str.length - 1;
+    let strArr = [...str];
+
+    console.log([...strArr].join(''));
+
+    while (start <= end) {
+
+        let temp;
+
+        if (testChar(strArr[start]) && testChar(strArr[end])) {
+            start += 1;
+            end -= 1;
+        }
+
+        if (testChar(strArr[start]) && !testChar(strArr[end])) {
+            start += 1;
+        }
+
+        if (!testChar(strArr[start]) && testChar(strArr[end])) {
+            end -= 1
+        }
+
+        if (!testChar(strArr[start]) && !testChar(strArr[end])) {
+            temp = strArr[end];
+            strArr[end] = strArr[start];
+            strArr[start] = temp;
+            start += 1;
+            end -= 1;
+        }
+        
+    }
+
+    console.log([...strArr].join(''));
+
+}
+
+reverseStr('asdfasd/.,fasdtgeg,(*)(&)&&)&*^LKHOGHOKJuhfufru(*&^&*&%*%');
+
+reverseStr('a@a#a$b%b^c&c*c(ddd!fff');
+
+reverseStr('Ab,c,de!$');
+
+// 02. All Possible Palindromic Partitions
+
+allPossiblePalindromicPartitions = (arr) => {
+    console.log('@reverseArr...');
+    let results = [];
+    return results;
+}
+
+// 03. Count triplets with sum smaller than a given value
+
+countTriplets = (arr) => {
+    console.log('@reverseArr...');
+    let results = [];
+    return results;
+}
+
+// 04. Convert array into Zig-Zag fashion
+
+zigZagArr = (arr) => {
+    console.log('@zigZagArr...');
+    let results = [];
+    return results;
+}
+
+// 05. Generate all possible sorted arrays from alternate elements of two given sorted arrays
+
+
+
+// 06. Pythagorean Triplet in an array
+
+
+
+// 07. Length of the largest subarray with contiguous elements
+
+
+
+// 08. Find the smallest positive integer value that cannot be represented as sum of any subset of a given array
+
+
+
+// 09. Smallest subarray with sum greater than a given value
+
+
+
+// 10. Stock Buy Sell to Maximize Profit
